382576de377cbf90a07055188ed916443854dbcd
Leo jaime <anderson@gmail.com>
2025-04-23 00:13:59 -0300

    feat: adiciona campos data_termino, motivo_saida, origem e plano_pagamento no modelo de alunos

--- src/models/aluno.js
+++ src/models/aluno.js
@@ -30,6 +30,66 @@ const AlunoModel = (sequelize) => {
     telefone: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('ativo', 'inativo'),
+      defaultValue: 'ativo',
+    },
+    data_matricula: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },    
+    data_termino: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    motivo_saida: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    endereco: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    bairro: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cidade: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    estado: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cep: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    rg: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    contato_emergencia_nome: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    contato_emergencia_telefone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    plano_pagamento: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    origem: {
+      type: DataTypes.STRING,
+      allowNull: false,
     }
   });
 };
